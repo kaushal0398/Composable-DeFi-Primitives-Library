01c0e858ce7e476111c521a0c8578c76cc830a15
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-10-18 00:10:32 +0530

    initial commit

---
+++ tests/staking.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { PublicKey, SystemProgram } from '@solana/web3.js';
+import { assert } from 'chai';
+
+describe('Staking Tests', () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Staking as Program<Staking>;
+
+  let stakingAccount: anchor.web3.Keypair;
+  let lpTokenAccount: anchor.web3.Keypair;
+  let rewardTokenAccount: anchor.web3.Keypair;
+  let userStakeAccount: anchor.web3.Keypair;
+    )
+}
