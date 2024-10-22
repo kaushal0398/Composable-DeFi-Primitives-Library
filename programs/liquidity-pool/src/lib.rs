c3f3d461aff98d3d70015779afc93c39a9d9c347
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-10-22 11:08:35 +0530

    initial commit

--- programs/liquidity-pool/src/lib.rs
+++ programs/liquidity-pool/src/lib.rs
@@ -24,9 +24,6 @@ pub mod liquidity_pool {
         token::transfer(ctx.accounts.transfer_b_ctx(), amount_b)?;
         pool.token_b_reserve += amount_b;
 
-        let lp_amount = calculate_lp_tokens(pool, amount_a, amount_b);
-        pool.total_lp_supply += lp_amount;
-
         Ok(())
     }
 
